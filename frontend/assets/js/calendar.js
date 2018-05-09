3d8f1e8a4a5f3a5a6b363ac761d53b90bb7d059a
wanghanosng <wanghansongbupt@163.com>
2018-05-10 00:04:31 +0800

    日历部分组件化，还未完成

--- frontend/assets/js/calendar.js
+++ frontend/assets/js/calendar.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 var dat = new Date(); //当前时间
 var nianD = dat.getFullYear();//当前年份
 var yueD = dat.getMonth(); //当前月
@@ -41,67 +40,17 @@ function add(){
   }
 
 }
-=======
-var nowtime = new Date(); //当前时间 
-var yearD = nowtime.getFullYear();//当前年份 
-var monthD = nowtime.getMonth(); //当前月 
-var dayD = nowtime.getDate(); //当前天
-window.onload=function(){ 
-  document.getElementById("date").addEventListener("click",function(e){
-    alert("调用ajax"+this.innerText);
-  })
-  add();  
-} 
-function add(){
-  //console.log("add"); 
-  document.getElementById('date').innerHTML = ""; 
-  var yearN = nowtime.getFullYear();//当前年份 
-  var monthN = nowtime.getMonth(); //当前月 
-  var dayN = nowtime.getDate(); //当前天 
-  var arr=["一月","二月","三月","四月","五月","六月","七月","八月","九月","十月","十一月","十二月"]; 
-  document.getElementById('nian').innerText = yearN; 
-  document.getElementById('yue').innerText = arr[monthN]; 
-  var setDat = new Date(yearN,monthN + 1,1 - 1);  
-  var setTian = setDat.getDate(); //获取 当前月最后一天 
-  var setZhou = new Date(yearN,monthN,1).getDay(); 
-  
-  for(var i=0;i<setZhou ;i++){ 
-    var li=document.createElement('li'); 
-    document.getElementById('date').appendChild(li); 
-  } 
-  
-  for(var i=1;i<=setTian;i++){ 
-    var li=document.createElement('li'); 
-    li.innerText = i; 
-    if(yearN == yearD && monthN == monthD && i == dayD){ 
-      li.className = "active";
-      //调用后台借口
-    }else{
-      li.className = "hover"; 
-    } 
-    document.getElementById('date').appendChild(li); 
-  } 
-    
-} 
->>>>>>> 029b66f4445eff8017cf95b57ceede3bf372083f
 function next(){
   document.getElementById("next").onclick = function(){
     //console.log("add");
     //调用接口返回json
-<<<<<<< HEAD
     dat.setMonth(dat.getMonth() + 1); //当点击下一个月时 对当前月进行加1;
     add(); //重新执行渲染 获取去 改变后的 年月日 进行渲染;
   };
-=======
-    nowtime.setMonth(nowtime.getMonth() + 1); //当点击下一个月时对当前月进行加1; 
-    add();  
-  }; 
->>>>>>> 029b66f4445eff8017cf95b57ceede3bf372083f
 }
 function prev(){
   document.getElementById("prev").onclick = function(){
     //调用接口返回json
-<<<<<<< HEAD
     dat.setMonth(dat.getMonth() - 1); //与下一月 同理
     add();
   };
@@ -111,9 +60,3 @@ var date=getElementById('date');
 date.onclick=function(event) {
   var day=event.target.innerText;
 }
-=======
-    nowtime.setMonth(nowtime.getMonth() - 1); // 
-    add();   
-  };
-}
->>>>>>> 029b66f4445eff8017cf95b57ceede3bf372083f
